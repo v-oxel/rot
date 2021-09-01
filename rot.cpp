6330a5bb24b417c27fab317dc0dbb541a2d4a1ac
v-oxel <61062921+v-oxel@users.noreply.github.com>
2021-09-01 18:00:36 +0300

    Add files via upload

--- rot.cpp
+++ rot.cpp
@@ -4,10 +4,10 @@
 
 #define ALPHABET_ENGLISH_LENGTH 26
 
-const char alphabet_english_upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 // [65..90]
-const char alphabet_english_lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+const char alphabet_english_upper[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 // [97..122]
+const char alphabet_english_lower[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 char get_char(int rot, char c)
 {
@@ -36,34 +36,42 @@ std::string get_output(int rot, std::string input)
 
 int main(int argc, char const *argv[])
 {
-    if (argc > 3)
+    switch (argc)
     {
-        std::cout << "Invalid number of parameters\nUsage: rot [N] [INPUT] to get ROT-N of input\n       rot [INPUT] to get all ROTs of input\n";
-        return -1;
-    }
-
-    int rot = 0;
-    std::string input;
-
-    if (argc == 1)
-    {
+    case 1:
+        {
+        int rot;
+        std::string input;
         std::cout << "rot >> ";
         std::cin >> rot;
         std::cin.ignore(32767, '\n');
         std::cout << "input >> ";
         getline(std::cin, input);
-    }
-    else if (argc == 2)
-    {
+        std::cout << get_output(rot, input) << '\n';
+        break;
+        }
+
+    case 2:
+        {
         for (int i = 0; i < 26; i++)
             std::cout << "ROT" << i << (i < 10 ? "   " : "  ") << get_output(i, argv[1]) << '\n';
-    }
-    else
-    {
+        break;
+        }
+
+    case 3:
+        {
+        int rot;
         std::stringstream ss(argv[1]);
         ss >> rot;
-        input = argv[2];
-        std::cout << get_output(rot, input) << '\n';
+        std::cout << get_output(rot, argv[2]) << '\n';
+        break;
+        }
+
+    default:
+        {
+        std::cout << "Invalid number of parameters\nUsage: rot [N] [INPUT] to get ROT-N of input\n       rot [INPUT] to get all ROTs of input\n";
+        break;
+        }
     }
 
     return 0;
